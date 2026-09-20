face7863e648b4545458b0b947561fa8718d0f39
Santiago <125106450+Danvsm@users.noreply.github.com>
2026-09-19 23:00:32 -0300

    Rename default guild title

---
+++ supabase/migrations/20260920020020_rename_default_guild_title.sql
@@ -0,0 +1,8 @@
+update public.cosmetics
+set
+  name = 'Guilda sem nome',
+  description = 'Ainda não possui uma guilda definida.',
+  icon = 'shield',
+  color = '#C8A96B'
+where id = '89eb651b-0ec0-4f4f-a4a2-bcafc20c786d'
+  and kind = 'title';
