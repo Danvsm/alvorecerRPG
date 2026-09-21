f0975f59f27867ade8af3f0e0aae0a4c2beb9e16
Santiago <125106450+Danvsm@users.noreply.github.com>
2026-09-21 17:03:17 -0300

    Pass master access to community inbox

--- components/CommunityPanel.tsx
+++ components/CommunityPanel.tsx
@@ -607,6 +607,7 @@ export default function CommunityPanel({
         <CommunityInbox
           campaign={campaign}
           actor={actor}
+          master={master}
           identities={identities}
           cosmetics={cosmetics}
           equipment={equipment}
